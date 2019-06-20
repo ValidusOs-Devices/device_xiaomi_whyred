cb3bc50e1d9914aadcfca2017429507e664ec550
Sajidjnl <syedsajid341@gmail.com>
2019-06-20 19:16:50 +0000

    whyred:Validus Up

--- validus_whyred.mk
+++ validus_whyred.mk
@@ -23,15 +23,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
 # Inherit from custom vendor
-$(call inherit-product, vendor/MiuiCamera/config.mk)
 $(call inherit-product-if-exists, vendor/xiaomi/Gcam/config.mk)
 
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/syberia/common.mk)
+$(call inherit-product, vendor/validus/common_full_phone.mk)
 
-PRODUCT_NAME := syberia_whyred
+PRODUCT_NAME := alidus_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
@@ -42,10 +40,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="whyred" \
     PRODUCT_NAME="whyred" \
+    DEVICE_MAINTAINER="Syed Sajid (ISyedSajid)" \
     PRIVATE_BUILD_DESC="whyred-user 9 PKQ1.180904.001 V10.3.1.0.PEIMIXM release-keys"
 
 BUILD_FINGERPRINT := "xiaomi/whyred/whyred:9/PKQ1.180904.001/V10.3.1.0.PEIMIXM:user/release-keys"
 
 TARGET_VENDOR := Xiaomi
-
-SYBERIA_BUILD_TYPE := OFFICIAL
