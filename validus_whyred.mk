ed97129587c1aabd5b189a7089cf408160431c9d
Sajidjnl <syedsajid341@gmail.com>
2019-07-05 09:36:08 +0000

    whyred:fix derp

--- validus_whyred.mk
+++ validus_whyred.mk
@@ -40,7 +40,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="whyred" \
     PRODUCT_NAME="whyred" \
-    DEVICE_MAINTAINER="Syed Sajid (ISyedSajid)" \
+    DEVICE_MAINTAINERS="Syed Sajid (ISyedSajid)" \
     PRIVATE_BUILD_DESC="whyred-user 9 PKQ1.180904.001 V10.3.1.0.PEIMIXM release-keys"
 
 BUILD_FINGERPRINT := "xiaomi/whyred/whyred:9/PKQ1.180904.001/V10.3.1.0.PEIMIXM:user/release-keys"
